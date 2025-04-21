e0dc1df5086ce51459f8cf810ffcbe772d8fefa2
wenxuan <deltaleung3@gmail.com>
2025-04-22 00:19:51 +0800

    HW

---
+++ HW55.java
@@ -0,0 +1,41 @@
+import java.util.PriorityQueue;
+
+class HW55 {
+    public boolean canJump(int[] nums) {
+        // row: (f, g + f)
+        PriorityQueue<int[]> rowH = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int len = nums.length;
+        int f = 0;
+
+        for (int i = 0; i < len; ++i) {
+            while (!rowH.isEmpty() && rowH.peek()[1] < i) {
+                rowH.poll();
+            }
+
+            f = Integer.MAX_VALUE;
+            if (i == 0) {
+                rowH.add(new int[] { 1, i + nums[i] });
+                f = 1;
+            }
+
+            if (!rowH.isEmpty()) {
+                f = rowH.peek()[0] + 1;
+            }
+
+            if (f < Integer.MAX_VALUE) {
+                rowH.add(new int[] { f, i + nums[i] });
+            }
+        }
+
+        return f != Integer.MAX_VALUE;
+    }
+}
+
+class Main_HW55 {
+    public static void main(String[] args) {
+        int[] nums = new int[] { 2, 3, 1, 1, 4 };
+        HW55 solution = new HW55();
+        boolean res = solution.canJump(nums);
+        System.out.println(res);
+    }
+}
